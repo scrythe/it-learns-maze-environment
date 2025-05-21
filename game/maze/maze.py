30f51cf149e784ef6d401d4cbcf8126aa3957fae
Magomed Alimkhanov <scrythepvp@gmail.com>
2025-05-21 22:14:14 +0200

    feat: possibly complete wilson graph gen

--- game/maze/maze.py
+++ game/maze/maze.py
@@ -11,57 +11,112 @@ class Direction(Enum):
 
 
 def generate_maze_structure(size: int):
-    maze_graph = _generate_maze_graph(size)
+    maze_graph = generate_maze_graph(size)
 
 
-def _generate_maze_graph(size: int):
+def generate_maze_graph(size: int):
+    total_cells_amount = size * size
+    # Total cellls amount times two because it does not only contain cells but also stop sequence
+    # Size in most cases smaller but just incase (worst case scenario total_cells_amount*2-1)
+    maze_graph = np.zeros((total_cells_amount * 2, 2), dtype=np.int32)
     visited_cells = np.zeros((size, size), dtype=np.int32)
-    visited_cells_i = 0
-    total_amount_cells = size * size - 1
-    maze_graph = np.array([])
-    stack = np.zeros((total_amount_cells - visited_cells_i, 2), dtype=np.int32)
-    while visited_cells_i < total_amount_cells:
+    current_cell_index = 0
+    visited_cells_index = 0
+
+    while current_cell_index < total_cells_amount - 1:
+        current_sub_graph_cells = np.zeros((size, size), dtype=np.int32)
         current_cell = np.random.randint(0, size, 2)
-        stack[visited_cells_i] = current_cell
-        _make_cell_visited(visited_cells, current_cell)
-        visited_cells_i += 1
+        current_cell_index, visited_cells_index = add_cell_to_lists(
+            maze_graph,
+            visited_cells,
+            current_sub_graph_cells,
+            current_cell,
+            current_cell_index,
+            visited_cells_index,
+        )
         while True:
-            if visited_cells_i < total_amount_cells:
-                stack.resize((visited_cells_i - 1, 2))
-                stack
-            possible_directions = _get_possible_directions(current_cell, size)
-            rand_direction_index = np.random.randint(0, len(possible_directions))
-            direction = possible_directions[rand_direction_index]
-            new_cell = cell_goto_direction(current_cell, direction)
-            if _check_cell_visited(visited_cells, new_cell):
-                break
-            stack[visited_cells_i] = new_cell
-            _make_cell_visited(visited_cells, current_cell)
+            if visited_cells_index >= total_cells_amount - 1:
+                return maze_graph
+            rand_direction = get_random_directions(current_cell, size)
+            new_cell = cell_goto_direction(current_cell, rand_direction)
+            if check_cell_inside_list(current_sub_graph_cells, new_cell):
+                current_cell_index, visited_cells_index = reverse_graph_to_cell(
+                    maze_graph,
+                    visited_cells,
+                    current_sub_graph_cells,
+                    current_cell,
+                    current_cell_index,
+                    visited_cells_index,
+                )
+                continue
+            current_cell_index, visited_cells_index = add_cell_to_lists(
+                maze_graph,
+                visited_cells,
+                current_sub_graph_cells,
+                new_cell,
+                current_cell_index,
+                visited_cells_index,
+            )
             current_cell = new_cell
-            visited_cells_i += 1
-    stack.resize((visited_cells_i - 1, 2))
-    print(maze_graph)
+            if check_cell_inside_list(visited_cells, new_cell):
+                maze_graph[current_cell_index] = [-1, -1]
+                current_cell_index += 1
+                break
+    return maze_graph
 
 
-def _get_possible_directions(current_cell: npt.NDArray, size: int):
-    possiblle_directions: list[Direction] = []
-    if current_cell[1] > 0:
-        possiblle_directions.append(Direction.UP)
-    if current_cell[0] < size - 1:
-        possiblle_directions.append(Direction.RIGHT)
-    if current_cell[1] < size - 1:
-        possiblle_directions.append(Direction.DOWN)
-    if current_cell[0] > 0:
-        possiblle_directions.append(Direction.LEFT)
-    return possiblle_directions
+def reverse_graph_to_cell(
+    maze_graph: npt.NDArray,
+    visited_cells: npt.NDArray,
+    current_sub_graph_cells: npt.NDArray,
+    target_cell: npt.NDArray,
+    cell_index: int,
+    visited_cells_index: int,
+):
+    current_cell = maze_graph[cell_index]
+    while (target_cell != current_cell).all():
+        visited_cells[current_cell[1]][current_cell[0]] = 0
+        current_sub_graph_cells[current_cell[1]][current_cell[0]] = 1
+        cell_index -= 1
+        visited_cells_index -= 1
+        current_cell = maze_graph[cell_index]
+    return cell_index, visited_cells_index
 
 
-def _make_cell_visited(visited_cells: npt.NDArray, cell: npt.NDArray):
+def add_cell_to_lists(
+    maze_graph: npt.NDArray,
+    visited_cells: npt.NDArray,
+    current_sub_graph_cells: npt.NDArray,
+    cell: npt.NDArray,
+    cell_index: int,
+    visited_cells_index: int,
+):
     visited_cells[cell[1]][cell[0]] = 1
+    current_sub_graph_cells[cell[1]][cell[0]] = 1
+    maze_graph[cell_index] = cell
+    cell_index += 1
+    visited_cells_index += 1
+    return cell_index, visited_cells_index
 
 
-def _check_cell_visited(visited_cells: npt.NDArray, cell: npt.NDArray):
-    return visited_cells[cell[1]][cell[0]] == 1
+def get_random_directions(current_cell: npt.NDArray, size: int):
+    possible_directions: list[Direction] = []
+    if current_cell[1] > 0:
+        possible_directions.append(Direction.UP)
+    if current_cell[0] < size - 1:
+        possible_directions.append(Direction.RIGHT)
+    if current_cell[1] < size - 1:
+        possible_directions.append(Direction.DOWN)
+    if current_cell[0] > 0:
+        possible_directions.append(Direction.LEFT)
+
+    rand_direction_index = np.random.randint(0, len(possible_directions))
+    rand_direction = possible_directions[rand_direction_index]
+    return rand_direction
+
+
+def check_cell_inside_list(list: npt.NDArray, cell: npt.NDArray):
+    return list[cell[1]][cell[0]] == 1
 
 
 def cell_goto_direction(current_cell: npt.NDArray, direction: Direction):
@@ -78,43 +133,4 @@ def cell_goto_direction(current_cell: npt.NDArray, direction: Direction):
 
 
 if __name__ == "__main__":
-
-    def test_make_cell_visited():
-        visited_cells = np.zeros((4, 4), dtype=np.int32)
-        cell = (1, 2)
-        _make_cell_visited(visited_cells, cell)
-        correct_visited_cells = np.array(
-            [
-                [0, 0, 0, 0],
-                [0, 0, 0, 0],
-                [0, 1, 0, 0],
-                [0, 0, 0, 0],
-            ]
-        )
-        if (visited_cells == correct_visited_cells).all():
-            print("Test successful")
-            return
-        print("Test failed")
-
-    def test_get_possible_directions():
-        current_cell = (0, 0)
-        possible_directiohns = _get_possible_directions(current_cell, 5)
-        if (
-            Direction.UP in possible_directiohns
-            or Direction.LEFT in possible_directiohns
-        ):
-            print("Test failed")
-            return
-        current_cell = (4, 4)
-        possible_directiohns = _get_possible_directions(current_cell, 5)
-        if (
-            Direction.RIGHT in possible_directiohns
-            or Direction.DOWN in possible_directiohns
-        ):
-            print("Test failed")
-            return
-        print("Test successful")
-
-    # test_make_cell_visited()
-    # test_get_possible_directions()
-    _generate_maze_graph(10)
+    generate_maze_graph(10)
