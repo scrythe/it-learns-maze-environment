60ea2e4556fc1e060621e05e05d386bfb226cf97
Magomed Alimkhanov <scrythepvp@gmail.com>
2025-05-25 01:29:54 +0200

    fix: fix raycaster a bit, make it work

--- game/raycaster.py
+++ game/raycaster.py
@@ -15,57 +15,50 @@ class Raycaster:
     def cast_single_ray(
         self, maze_structure: npt.NDArray, player_pos: tuple[float, float], angle: float
     ):
-        player_pos_normal = np.array(player_pos) / 40
-        ray_pos = player_pos_normal.copy()
+        ray_pos = np.array(player_pos) / 40
+        ray_maze_pos = np.array([int(ray_pos[0]), int(ray_pos[1])])
         ray_dir_x = math.cos(angle)
         ray_dir_y = math.sin(angle)
         if ray_dir_x == 0:
             ray_length_step_x = 1.0
+            ray_length_step_y = 0.0
         else:
             ray_length_step_x = math.sqrt(1 + math.pow((ray_dir_y / ray_dir_x), 2))
         if ray_dir_y == 0:
+            ray_length_step_x = 0.0
             ray_length_step_y = 1.0
         else:
             ray_length_step_y = math.sqrt(1 + math.pow((ray_dir_x / ray_dir_y), 2))
         # looking left
         if math.pi / 2 < angle < 3 * math.pi / 2:
             ray_step_x = -1
-            ray_length_x = (
-                player_pos_normal[0] - int(player_pos_normal[0])
-            ) * ray_length_step_x
+            ray_length_x = (ray_pos[0] - ray_maze_pos[0]) * ray_length_step_x
         # looking right
         else:
             ray_step_x = 1
-            ray_length_x = (
-                int(player_pos_normal[0]) + 1 - player_pos_normal[0]
-            ) * ray_length_step_x
+            ray_length_x = (ray_maze_pos[0] + 1 - ray_pos[0]) * ray_length_step_x
         # looking down
         if 0 < angle < math.pi:
             ray_step_y = 1
-            ray_length_y = (
-                int(player_pos_normal[1] + 1) - player_pos_normal[1]
-            ) * ray_length_step_y
+            ray_length_y = (ray_maze_pos[1] + 1 - ray_pos[1]) * ray_length_step_y
         # looking up
         else:
             ray_step_y = -1
-            ray_length_y = (
-                player_pos_normal[1] - int(player_pos_normal[1])
-            ) * ray_length_step_y
+            ray_length_y = (ray_pos[1] - ray_maze_pos[1]) * ray_length_step_y
         collision = False
         ray_length = 0
         # ray_pos -= 1
         while not collision:
             if ray_length_x > ray_length_y:
-                ray_pos[1] += ray_step_y
+                ray_maze_pos[1] += ray_step_y
                 ray_length = ray_length_y
                 ray_length_y += ray_length_step_y
             else:
-                ray_pos[0] += ray_step_x
+                ray_maze_pos[0] += ray_step_x
                 ray_length = ray_length_x
                 ray_length_x += ray_length_step_x
-            ray_maze_pos = int(ray_pos[0]), int(ray_pos[1])
             if maze_structure[ray_maze_pos[1], ray_maze_pos[0]] == WALL:
-                print(ray_maze_pos)
                 collision = True
-        return ray_pos * 40
-        # print(ray_length)
+        print(angle)
+        ray_end = player_pos + ray_length * np.array([ray_dir_x, ray_dir_y]) * 40
+        return ray_end
